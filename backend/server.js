5b480fb37225fd7c4e43d67008549d03b9039948
Muhammed <muhammedrefaat2020@gmail.com>
2025-11-22 15:47:35 +0200

    Install TypeScript and types as dev dependencies in your project root and other folders

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+app.use("api/auth",authRoutes)
 app.listen(PORT, () => {
   console.log("====================================");
   console.log(`Server is running on http://localhost:${PORT}`);
