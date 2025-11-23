9e5b9d4e5c3f913db305fc40827fb6bfcb33f0a4
Muhammed <muhammedrefaat2020@gmail.com>
2025-11-24 00:26:00 +0200

    Add Product feature and cloudinary service

--- backend/middleware/auth.middleware.ts
+++ backend/middleware/auth.middleware.ts
@@ -3,29 +3,52 @@ import User from "../models/user.model.js";
 
 export const protectRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
+    const token = req.cookies.accessToken;
 
     if (!token) {
-      return res.status(401).json({ message: "Unauthorized - No token provided" });
+      return res
+        .status(401)
+        .json({ message: "Unauthorized - No token provided" });
     }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    try {
+      const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
 
-    if (!decoded) {
-      return res.status(401).json({ message: "Unauthorized - Invalid token" });
+      if (!decoded) {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - Invalid token" });
+      }
+
+      const user = await User.findById(decoded.userId).select("-password");
+
+      if (!user) {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - User not found" });
+      }
+
+      req.user = user;
+
+      next();
+    } catch (error) {
+      if (error == "TokenExpiredError") {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized - Access Token expired" });
+      }
+      throw error;
     }
-
-    const user = await User.findById(decoded.userId).select("-password");
-
-    if (!user) {
-      return res.status(401).json({ message: "Unauthorized - User not found" });
-    }
-
-    req.user = user;
-
-    next();
   } catch (error) {
     console.log("Error in protectRoute middleware", error);
     res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const adminRoute = (req, res, next) => {
+  if (req.user && req.user.role == "admin") {
+    next();
+  } else {
+    return res.status(403).json({ message: "Access denied - Admin Only" });
   }
 };
